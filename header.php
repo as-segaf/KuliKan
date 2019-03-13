5ee2e57ce17564f4f641c0c057541016e0abc7a0
as-segaf <ziyanassegaf20@gmail.com>
2019-03-13 16:05:43 +0700

    add header.php & edit about us

---
+++ header.php
@@ -0,0 +1,61 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="codepixer">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>KuliKan</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/nice-select.css">					
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>
+
+			  <header id="header" id="home">
+			    <div class="container">
+			    	<div class="row align-items-center justify-content-between d-flex">
+				      <div id="logo">
+				        <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+				      </div>
+				      <nav id="nav-menu-container">
+				        <ul class="nav-menu">
+				          <li class="menu-active"><a href="index.html">Home</a></li>
+				          <li><a href="about-us.php">Tentang kami</a></li>
+				          <li><a href="category.html">Pilihan Jasa</a></li>
+				          <li><a href="price.html">Partner Kami</a></li>
+				          <li><a href="blog-home.html">Blog</a></li>
+				          <li><a href="contact.html">Kontak</a></li>
+				          <li class="menu-has-children"><a href="">Pages</a>
+				            <ul>
+								<li><a href="elements.html">elements</a></li>
+								<li><a href="search.html">search</a></li>
+								<li><a href="single.html">single</a></li>
+				            </ul>
+				          </li>
+				          <li><a class="ticker-btn" href="#">Signup</a></li>
+				          <li><a class="ticker-btn" href="#">Login</a></li>				          				          
+				        </ul>
+				      </nav><!-- #nav-menu-container -->		    		
+			    	</div>
+			    </div>
+			  </header><!-- #header -->
