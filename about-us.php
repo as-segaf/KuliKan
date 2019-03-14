1a60adfe846b77e255523b64aebcb856f2f8a6c8
as-segaf <ziyanassegaf20@gmail.com>
2019-03-14 08:23:43 +0700

    edit about-us

--- about-us.php
+++ about-us.php
@@ -203,17 +203,17 @@
 			</section>
 			<!-- End team Area -->			
 
-
+			
 			<!-- Start callto-action Area -->
-			<section class="callto-action-area section-gap">
+			<section class="callto-action-area section-gap" id="join">
 				<div class="container">
 					<div class="row d-flex justify-content-center">
 						<div class="menu-content col-lg-9">
 							<div class="title text-center">
-								<h1 class="mb-10 text-white">Join us today without any hesitation</h1>
-								<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-								<a class="primary-btn" href="#">I am a Candidate</a>
-								<a class="primary-btn" href="#">We are an Employer</a>
+								<h1 class="mb-10 text-white">Bergabunglah bersama kami</h1>
+								<p class="text-white">Kami akan membantu menemukan jasa yang anda butuhkan</p>
+								<a class="primary-btn" href="#">Daftar sebagai customer</a>
+								<a class="primary-btn" href="#">Daftar sebagai partner KuliKan</a>
 							</div>
 						</div>
 					</div>	
