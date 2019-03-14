687548718eeec60eae79e45ff275388d75a28125
as-segaf <ziyanassegaf20@gmail.com>
2019-03-14 09:08:32 +0700

    change file's type & edit

--- contact.php
+++ contact.php
@@ -1,65 +1,6 @@
-	<!DOCTYPE html>
-	<html lang="zxx" class="no-js">
-	<head>
-		<!-- Mobile Specific Meta -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!-- Favicon-->
-		<link rel="shortcut icon" href="img/fav.png">
-		<!-- Author Meta -->
-		<meta name="author" content="codepixer">
-		<!-- Meta Description -->
-		<meta name="description" content="">
-		<!-- Meta Keyword -->
-		<meta name="keywords" content="">
-		<!-- meta character set -->
-		<meta charset="UTF-8">
-		<!-- Site Title -->
-		<title>Job Listing</title>
-
-		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
-			<!--
-			CSS
-			============================================= -->
-			<link rel="stylesheet" href="css/linearicons.css">
-			<link rel="stylesheet" href="css/font-awesome.min.css">
-			<link rel="stylesheet" href="css/bootstrap.css">
-			<link rel="stylesheet" href="css/magnific-popup.css">
-			<link rel="stylesheet" href="css/nice-select.css">					
-			<link rel="stylesheet" href="css/animate.min.css">
-			<link rel="stylesheet" href="css/owl.carousel.css">
-			<link rel="stylesheet" href="css/main.css">
-		</head>
-		<body>
-
-			  <header id="header" id="home">
-			    <div class="container">
-			    	<div class="row align-items-center justify-content-between d-flex">
-				      <div id="logo">
-				        <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
-				      </div>
-				      <nav id="nav-menu-container">
-				        <ul class="nav-menu">
-				          <li class="menu-active"><a href="index.html">Home</a></li>
-				          <li><a href="about-us.html">About Us</a></li>
-				          <li><a href="category.html">Category</a></li>
-				          <li><a href="price.html">Price</a></li>
-				          <li><a href="blog-home.html">Blog</a></li>
-				          <li><a href="contact.html">Contact</a></li>
-				          <li class="menu-has-children"><a href="">Pages</a>
-				            <ul>
-								<li><a href="elements.html">elements</a></li>
-								<li><a href="search.html">search</a></li>
-								<li><a href="single.html">single</a></li>
-				            </ul>
-				          </li>
-				          <li><a class="ticker-btn" href="#">Signup</a></li>
-				          <li><a class="ticker-btn" href="#">Login</a></li>				          				          
-				        </ul>
-				      </nav><!-- #nav-menu-container -->		    		
-			    	</div>
-			    </div>
-			  </header><!-- #header -->
-
+			<?php 
+				include'header.php';
+			 ?>
 
 			<!-- start banner Area -->
 			<section class="banner-area relative" id="home">	
@@ -83,9 +24,9 @@
 					<div class="row">
 						<div class="map-wrap" style="width:100%; height: 445px;" id="map"></div>
 						<div class="col-lg-4 d-flex flex-column">
-							<a class="contact-btns" href="#">Submit Your CV</a>
-							<a class="contact-btns" href="#">Post New Job</a>
-							<a class="contact-btns" href="#">Create New Account</a>
+							<a class="contact-btns" href="#">Daftar sebagai customer</a>
+							<a class="contact-btns" href="#">Daftar sebagai partner KuliKan.com</a>
+							<a class="contact-btns" href="#">Sudah punya akun?</a>
 						</div>
 						<div class="col-lg-8">
 							<form class="form-area " id="myForm" action="mail.php" method="post" class="contact-form text-right">
@@ -97,7 +38,7 @@
 
 										<input name="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your subject'" class="common-input mb-20 form-control" required="" type="text">
 										<textarea class="common-textarea mt-10 form-control" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required=""></textarea>
-										<button class="primary-btn mt-20 text-white" style="float: right;">Send Message</button>
+										<button class="primary-btn mt-20 text-white" style="float: right;">Kirim Pesan ke KuliKan.com</button>
 										<div class="mt-20 alert-msg" style="text-align: left;"></div>
 									</div>
 								</div>
